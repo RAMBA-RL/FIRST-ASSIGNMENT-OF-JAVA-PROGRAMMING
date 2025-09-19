aa665e81ddb1ab56b5a5c51c037b6c169e2a5ec2
RAMBA-RL <leonoreramba@gmail.com>
2025-09-19 22:48:14 +0200

    Add files via upload

---
+++ ASSIGNMENT OF JAVA-19-09-2025/Square.java
@@ -0,0 +1,29 @@
+package com.shapes;
+
+public class Square {
+	private double side;
+
+
+	public Square(double side) {
+		
+		this.side = side;
+	}
+	public double getSide() {
+		return side;
+	}
+
+	public void setSide(double side) {
+		this.side = side;
+	}
+	public double calculateSquareArea(){
+		return this.side * this.side;
+	}
+	public double calculateSquarePerimeter(){
+		return this.side * 4;
+	}
+
+
+
+
+	}
+
