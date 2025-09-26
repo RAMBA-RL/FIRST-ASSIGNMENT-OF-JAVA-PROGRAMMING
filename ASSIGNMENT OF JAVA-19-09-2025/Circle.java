33640376c95b79cdaa04d63765abf5a184d3cc9b
RAMBA-RL <leonoreramba@gmail.com>
2025-09-26 17:45:38 +0200

    Add files via upload

---
+++ ASSIGNMENT OF JAVA-19-09-2025/Circle.java
@@ -0,0 +1,29 @@
+package com.shapes;
+
+	public class Circle {
+	private double r;
+
+
+	public Circle(double r) {
+		this.r = r;
+	}
+
+	public double getR() {
+		return r;
+	}
+
+	public void setR(double r) {
+		this.r = r;
+	}
+
+	public double calculateCircleArea(){
+		return Math.PI * Math.pow(r, 2);
+	}
+	public double calculateCircleCurcum(){
+		return 2 * (Math.PI * r);
+	}
+
+	}
+
+
+
